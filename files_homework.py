920725d217d7f86f83b573d4fdb7675d6505f562
Damir Dolotkazin <d.kott@yandex.ru>
2021-02-27 11:55:11 +0300

    completed tasks 1 and 2 from homework-basic_7

---
+++ files_homework.py
@@ -0,0 +1,26 @@
+import pprint
+
+def get_cook_book():
+    with open('recipes.txt', 'r', encoding="utf-8") as f:
+        tmp = f.read()
+
+    cook_book = {}
+    load_lst = tmp.split('\n\n')
+
+    for item in load_lst:
+        dish = item.split('\n')
+        dish_name = dish.pop(0)
+        dish.pop(0)
+        dish_recipe_tmp = []
+        for i in dish:
+            tmp_string = str(i)
+            tmp_ingridient = (tmp_string.split("|"))
+
+            dish_ingridient = {"ingredient_name": tmp_ingridient[0].strip(), 'quantity': tmp_ingridient[1].strip(),
+                               'measure': tmp_ingridient[2].strip()}
+            print(dish_ingridient)
+            dish_recipe_tmp.append(dish_ingridient)
+            cook_book[dish_name] = dish_recipe_tmp
+
+    return cook_book
+
---
+++ shop_list.py
@@ -0,0 +1,33 @@
+from files_homework import get_cook_book
+import pprint
+
+dishes = get_cook_book()
+
+
+def get_dish_list(dish_cook_book):
+    dish_list = list(dish_cook_book.keys())
+    return dish_list
+
+
+dish_lst = get_dish_list(dishes)
+
+
+def get_shop_list_by_dishes(dish, persons):
+    result = {}
+    for i in range(len(dish)):
+        a = dishes.get(dish[i])
+        for k in range(len(a)):
+            ingr = a[k].get('ingredient_name')
+            measure = a[k].get("measure")
+            quantity = (int(a[k].get("quantity"))) * persons
+            if ingr not in result:
+                result[ingr] = {"measure": measure, "quantity": quantity}
+            else:
+                existed_ingr = result.get(ingr)
+                tmp = existed_ingr.get('quantity') + quantity
+                # to_update = {"measure": measure, "quantity": tmp}
+                result[ingr] = {"measure": measure, "quantity": tmp}
+    return result
+
+
+pprint.pprint(get_shop_list_by_dishes(dish_lst, 3))
