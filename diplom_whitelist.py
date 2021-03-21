0d24b66dc281f966661bfbe7fc9315692c529b78
stAnger917 <72664930+stAnger917@users.noreply.github.com>
2021-03-21 11:20:02 +0300

    Update diplom_whitelist.py

--- diplom_whitelist.py
+++ diplom_whitelist.py
@@ -8,9 +8,7 @@ How to use
 
 Greetings! First you should to do - paste your yandex token from da https://yandex.ru/dev/disk/poligon/
 into variable yandex_token. VK_token - is common, just use it, no need to change.
-Then - just run the function vk_profile_photo_backup()
 
-I split the execution flow of the application on states for convenience.
 For logging I use logging module into the file logs.txt. Before running program - be sure that file exists.
 """
 
